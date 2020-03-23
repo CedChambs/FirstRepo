6ded7855ee032d697acc2488ebf9282df3de47ad
CedChambs github <cedrichamberland@hotmail.com>
2020-03-22 20:50:10 -0400

    add folder

---
+++ git demo/Exploratory Modeling TEST.R
@@ -0,0 +1,13 @@
+### Introduction to Data Science with R - Exploratory Modeling 2 ###
+
+install.packages("caret")
+install.packages("doSNOW")
+library(caret)
+library(doSNOW)
+
+# Using Single decision tree
+
+install.packages("rpart")
+install.packages("rpart.plot")
+library(rpart)
+library(rpart.plot)
